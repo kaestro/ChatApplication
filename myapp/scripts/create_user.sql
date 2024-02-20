12fddcbab422b0027615c8a10f7f271e5c7ad118
kaestro <didme654@snu.ac.kr>
2024-02-20 14:48:40 +0900

    Delayed sql update

--- myapp/scripts/create_user.sql
+++ myapp/scripts/create_user.sql
@@ -6,3 +6,6 @@ CREATE TABLE "user" (
 );
 
 ALTER TABLE "user" RENAME TO "users";
+
+ALTER TABLE "users" RENAME COLUMN "userName" TO "user_name";
+ALTER TABLE "users" RENAME COLUMN "emailAddress" TO "email_address";
