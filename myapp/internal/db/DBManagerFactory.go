4e85545c373f5ca076323e19b3c960d8c1635b5f
kaestro <didme654@snu.ac.kr>
2024-03-22 10:42:43 +0900

    DBManagerFactory refactored and test added

--- myapp/internal/db/DBManagerFactory.go
+++ myapp/internal/db/DBManagerFactory.go
@@ -10,9 +10,18 @@ const (
 	// 여기에 다른 데이터베이스 타입을 추가할 수 있습니다.
 )
 
-type DBManagerFactory struct{}
+type DBManagerFactory struct {
+	NewDBManager func(DBType) (DBManagerInterface, error)
+}
 
 func (f *DBManagerFactory) CreateDBManager(dbType DBType) (DBManagerInterface, error) {
+	if f.NewDBManager != nil {
+		return f.NewDBManager(dbType)
+	}
+	return f.createDefaultDBManager(dbType)
+}
+
+func (f *DBManagerFactory) createDefaultDBManager(dbType DBType) (DBManagerInterface, error) {
 	switch dbType {
 	case Postgres:
 		return NewPostgresManager()
