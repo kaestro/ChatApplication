444ce32763caf9507b910c305e1132f732ccfdbe
neuma573 <crye@kakao.com>
2024-03-25 16:15:10 +0900

    test: 로그인 및 로그인 실패 단위테스트 작성

--- myapp/tests/mock_tests/api/users/Login_test.go
+++ myapp/tests/mock_tests/api/users/Login_test.go
@@ -1,8 +1,53 @@
 package tests
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"myapp/api/handlers/user"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestLogIn(t *testing.T) {
-	// TODO
-	t.Log("Test not implemented.")
+
+	gin.SetMode(gin.TestMode)
+
+	t.Run("InvalidRequestBody", func(t *testing.T) {
+
+		router := gin.Default()
+
+		router.POST("/login", user.LogIn)
+
+		requestBody := bytes.NewBufferString(`{invalid json}`)
+		req, _ := http.NewRequest(http.MethodPost, "/login", requestBody)
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+}
+
+func TestLogIn_UserNotFound(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
+	router := gin.Default()
+	router.POST("/login", user.LogIn)
+
+	requestBody, _ := json.Marshal(map[string]string{
+		"emailAddress": "nonexistent@example.com",
+		"password":     "password",
+	})
+	req, _ := http.NewRequest(http.MethodPost, "/login", bytes.NewBuffer(requestBody))
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
 }
