86984f563650edc44eed0d4612a839f5dc4b1258
kaestro <didme654@snu.ac.kr>
2024-04-14 11:39:57 +0900

    jsonProperty와 typeAliasing 최상단에 고정

---
+++ myapp/jsonProperties/jsonProperties.go
@@ -0,0 +1,13 @@
+// myapp/jsonProperties/jsonProperties.go
+package jsonProperties
+
+const (
+	ID       = "id"
+	RoomName = "roomName"
+	UserName = "userName"
+	Content  = "content"
+
+	EmailAddress   = "emailAddress"
+	Password       = "password"
+	LoginSessionID = "loginSessionID"
+)
